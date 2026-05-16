02f3eec9f3c940fdbef75ee884f42e8d6dcc7171
Jeff Barriault <jeff@barriault.net>
2026-05-16 11:02:09 -0500

    Add Player::PlayerCharacterViewModel (no notes leak)

---
+++ app/view_models/player/player_character_view_model.rb
@@ -0,0 +1,10 @@
+module Player
+  class PlayerCharacterViewModel < ApplicationViewModel
+    expose :id
+    expose :name
+    expose :role
+    expose :class_name
+    expose :level
+    expose :pronouns
+  end
+end
