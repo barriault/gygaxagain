948ac238adae2396cb0c3023612c807a65e4c8dc
Jeff Barriault <jeff@barriault.net>
2026-05-16 12:41:02 -0500

    Play::ScenesController: auto-frame scene on first load (zero events)

--- app/controllers/play/scenes_controller.rb
+++ app/controllers/play/scenes_controller.rb
@@ -7,6 +7,15 @@ module Play
                  .scenes
                  .find(params[:id])
 
+      if @scene.events.empty?
+        narration = @scene.events.create!(
+          kind: "narration",
+          turn_number: 1,
+          payload: { "text" => "", "status" => "streaming", "trigger" => "framing" }
+        )
+        NarrationJob.perform_later(scene_id: @scene.id, narration_event_id: narration.id, trigger: "framing")
+      end
+
       render Play::Scenes::PlayComponent.new(scene: @scene)
     end
   end
